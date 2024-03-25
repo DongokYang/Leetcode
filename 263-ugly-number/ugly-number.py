46cdcab6f84d9f951437729874fc763509929bd1
rrc_f2023_Dongok Yang <143650239+DongokYang@users.noreply.github.com>
2024-03-25 15:13:44 -0500

    Time: 16 ms (55.22%) | Memory: 11.7 MB (42.72%) - LeetSync

---
+++ 263-ugly-number/ugly-number.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isUgly(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n==0:
+            return False
+
+        while True:
+            if n%2 ==0:
+                n /=2
+            elif n%3==0:
+                n /=3
+            elif n%5==0:
+                n/=5
+
+            else:
+                break
+        if n==1:
+            return True
+        else:
+            return False    
