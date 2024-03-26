a939197379bb1f8b8dc117a661fc5939812ed3d9
rrc_f2023_Dongok Yang <143650239+DongokYang@users.noreply.github.com>
2024-03-26 17:27:38 -0500

    Time: 49 ms (43.21%) | Memory: 16.5 MB (50.21%) - LeetSync

---
+++ 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        
+        cnt = 0
+
+        for i in nums:
+            if i <k:
+                cnt +=1
+
+        return cnt
