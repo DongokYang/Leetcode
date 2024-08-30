be965f3bdf80ac5870d320b0e6bf1eb60f604a08
rrc_f2023_Dongok Yang <143650239+DongokYang@users.noreply.github.com>
2024-08-31 05:54:07 +0900

    Time: 39 ms (65.37%) | Memory: 16.6 MB (28.62%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -1,16 +1,10 @@
-class Solution(object):
-    def kidsWithCandies(self, candies, extraCandies):
-        """
-        :type candies: List[int]
-        :type extraCandies: int
-        :rtype: List[bool]
-        """
-
-        maxnum = max(candies)
-        al = []
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        ansList = []
+        maxValue = max(candies)
         for i in candies:
-            if i+extraCandies >= maxnum:
-                al.append(True)
+            if i+extraCandies >= maxValue:
+                ansList.append(True)
             else:
-                al.append(False)
-        return al 
+                ansList.append(False)
+        return ansList
