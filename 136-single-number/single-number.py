a3158fe01558d238eee3d81cdd8023014788977c
rrc_f2023_Dongok Yang <143650239+DongokYang@users.noreply.github.com>
2024-09-03 13:50:33 +0900

    Time: 199 ms (14.94%) | Memory: 19.6 MB (6.10%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,13 +1,8 @@
-class Solution(object):
-    def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
         newlist = [0 for i in range(60000)]
         for ii in nums:
             newlist[ii+30000] +=1
         for i in range(len(newlist)):
             if newlist[i] == 1:
                 return i-30000
-    
