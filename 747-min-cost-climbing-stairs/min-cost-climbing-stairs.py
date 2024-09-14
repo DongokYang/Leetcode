5df4d2423c9bad0d6b94cdd4c46f4cc915a9d703
rrc_f2023_Dongok Yang <143650239+DongokYang@users.noreply.github.com>
2024-09-14 10:45:18 +0900

    Time: 64 ms (7.91%) | Memory: 16.7 MB (66.98%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,5 +1,6 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
         n = 2
         sm = 0
         cost.insert(0,0)
@@ -10,4 +11,3 @@ class Solution:
             n +=1
 
         return lst[-1]
-
