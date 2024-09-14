c10475ec0908ec646ed9322e2efee556080dcf6b
rrc_f2023_Dongok Yang <143650239+DongokYang@users.noreply.github.com>
2024-09-14 10:38:43 +0900

    Time: 49 ms (86.19%) | Memory: 16.6 MB (89.69%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+        n = 2
+        sm = 0
+        cost.insert(0,0)
+        cost.append(0)
+        lst = [cost[0],cost[1]]
+        while n < len(cost):
+            lst.append(min(lst[n-1] + cost[n],cost[n]+ lst[n-2]))
+            n +=1
+
+        return lst[-1]
