cdda91111c417e849c5acd66afc9a53dd1a3aa41
rrc_f2023_Dongok Yang <143650239+DongokYang@users.noreply.github.com>
2024-10-23 13:41:23 -0500

    Time: 10 ms (96.66%) | Memory: 23.1 MB (5.10%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,11 +1,11 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
         s = list(s)
         cleaned_string =''.join(char.lower() for char in s if char.isalnum())
+
+
+
+        
         if cleaned_string == cleaned_string[::-1]:
             return True
         else:
